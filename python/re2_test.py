rsc/re2

commit 33866f9d5dd055f66f4dda962acf92961f03fae0

Make module-level functions accept already compiled `pattern`s.
This is for slightly better compatibility with the `re` module.

Change-Id: Iad3ea646c6077a223423cbf6d244b752bca18569
Reviewed-on: https://code-review.googlesource.com/c/re2/+/60290
Reviewed-by: Perry Lorier <perryl@google.com>
Reviewed-by: Paul Wankadia <junyer@google.com>

python/re2.py
@@ -64,8 +64,12 @@ class Options(_re2.RE2.Options):
 
 
 def compile(pattern, options=None):
-  if not options:
-    options = Options()
+  if isinstance(pattern, _Regexp):
+    if options:
+      raise error('pattern is already compiled, so '
+                  'options may not be specified')
+    pattern = pattern._pattern
+  options = options or Options()
   values = tuple(getattr(options, name) for name in Options.NAMES)
   return _Regexp._make(pattern, values)
 
@@ -156,7 +160,7 @@ class _Regexp(object):
 
   def __getstate__(self):
     options = {name: getattr(self.options, name) for name in Options.NAMES}
-    return self.pattern, options
+    return self._pattern, options
 
   def __setstate__(self, state):
     pattern, options = state
@@ -503,8 +507,7 @@ class Set(object):
   __slots__ = ('_set')
 
   def __init__(self, anchor, options=None):
-    if not options:
-      options = Options()
+    options = options or Options()
     self._set = _re2.Set(anchor, options)
 
   @classmethod
@@ -551,8 +554,7 @@ class Filter(object):
     self._filter = _re2.Filter()
 
   def Add(self, pattern, options=None):
-    if not options:
-      options = Options()
+    options = options or Options()
     if isinstance(pattern, str):
       encoded_pattern = _encode(pattern)
       index = self._filter.Add(encoded_pattern, options)

python/re2_test.py
@@ -49,6 +49,13 @@ class Re2CompileTest(parameterized.TestCase):
   def test_compile(self, pattern, expected_groups, expected_groupindex):
     regexp = re2.compile(pattern)
     self.assertIs(regexp, re2.compile(pattern))  # cached
+    self.assertIs(regexp, re2.compile(regexp))  # cached
+    with self.assertRaisesRegex(re2.error,
+                                ('pattern is already compiled, so '
+                                 'options may not be specified')):
+      options = re2.Options()
+      options.log_errors = not options.log_errors
+      re2.compile(regexp, options=options)
     self.assertIsNotNone(regexp.options)
     self.assertEqual(expected_groups, regexp.groups)
     self.assertDictEqual(dict(expected_groupindex), regexp.groupindex)

commit 2cd408922905271455a9bd646e5e5791773f0af5

python: fix re2.search(compiled, text)
The old code was accidentally recompiling the pattern
and losing the options, both inefficient and incorrect.
Thanks to @MajorTanya for the excellent diagnosis.

This bug was introduced in CL 60290 and then copied
over from the Abseil branch.

Fixes #558.

python/re2.py
@@ -64,7 +64,7 @@ def compile(pattern, options=None):
     if options:
       raise error('pattern is already compiled, so '
                   'options may not be specified')
-    pattern = pattern._pattern
+    return pattern
   options = options or Options()
   values = tuple(getattr(options, name) for name in Options.NAMES)
   return _Regexp._make(pattern, values)

python/re2_test.py
@@ -315,6 +315,12 @@ class Re2RegexpTest(ReRegexpTest):
     re2.purge()
     self.assertEqual(re2._Regexp._make.cache_info().currsize, 0)
 
+  def test_options(self):
+    opt = re2.Options()
+    opt.case_sensitive = False
+    r = re2.compile('test', opt)
+    self.assertIsNotNone(r.search('TEST'))
+    self.assertIsNotNone(re2.search(r, 'TEST'))
 
 class Re2EscapeTest(parameterized.TestCase):
   """Contains tests that apply to the re2 module only.